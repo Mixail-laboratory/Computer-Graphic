5117e4b1520e750e1b837cb793ba6c5588a69782
Mixail-laboratory <m.solomin@g.nsu.ru>
2022-02-26 22:51:22 +0700

    Example from QtCreator

---
+++ ExampleTriangle/openglwindow.h
@@ -0,0 +1,45 @@
+#ifndef UNTITLED2_OPENGLWINDOW_H
+#define UNTITLED2_OPENGLWINDOW_H
+
+#include <QtGui/QWindow>
+#include <QtGui/QOpenGLFunctions>
+
+QT_BEGIN_NAMESPACE
+class QPainter;
+class QOpenGLContext;
+class QOpenGLPaintDevice;
+QT_END_NAMESPACE
+
+//! [1]
+class OpenGLWindow : public QWindow, protected QOpenGLFunctions
+{
+Q_OBJECT
+public:
+    explicit OpenGLWindow(QWindow *parent = 0);
+    ~OpenGLWindow();
+
+    virtual void render(QPainter *painter);
+    virtual void render();
+
+    virtual void initialize();
+
+    void setAnimating(bool animating);
+
+public slots:
+    void renderLater();
+    void renderNow();
+
+protected:
+    bool event(QEvent *event) override;
+
+    void exposeEvent(QExposeEvent *event) override;
+
+private:
+    bool m_animating;
+
+    QOpenGLContext *m_context;
+    QOpenGLPaintDevice *m_device;
+};
+
+
+#endif //UNTITLED2_OPENGLWINDOW_H
